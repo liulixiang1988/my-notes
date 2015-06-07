bb659ce8f9e8b64a63d33e26e48a698ed3105533
Liu Lixiang <liulixiang1988@gmail.com>
2015-06-07 14:40:13 +0800

    resue angular template, create angular service and module

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,5 @@
 (function(){
-app = angular.module('mynotes', ['ionic']);
+app = angular.module('mynotes', ['ionic', 'mynotes.notestore']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   //配置路由
@@ -8,60 +8,46 @@ app.config(function($stateProvider, $urlRouterProvider) {
     templateUrl: 'templates/list.html'
   });
 
+  $stateProvider.state('add',{
+    url: '/add',
+    templateUrl: 'templates/edit.html',
+    controller: 'AddCtrl'
+  });
+
   $stateProvider.state('edit',{
     url: '/edit/:noteId',
-    templateUrl: 'templates/edit.html'
+    templateUrl: 'templates/edit.html',
+    controller: 'EditCtrl'
   });
 
   $urlRouterProvider.otherwise('/list');
 });
 
-var notes = [
-    {
-      id: "1",
-      title: "第一个笔记",
-      description: "这是我的第一个笔记"
-    },
-    {
-      id: "2",
-      title: "第二个笔记",
-      description: "这是我的第二个笔记"
-    }
-  ];
-
-function getNote(noteId){
-  for(var i = 0; i < notes.length; i++){
-    if(notes[i].id === noteId){
-      return notes[i];
-    }
-  }
-  return undefined;
-}
-
-function updateNote(note){
-  for(var i = 0; i < notes.length; i++){
-    if(notes[i].id === note.id){
-      notes[i] = note;
-      return;
-    }
-  }
-}
-
-
-app.controller('ListCtrl', function($scope){
-  $scope.notes = notes;
+app.controller('ListCtrl', function($scope, NoteStore){
+  $scope.notes = NoteStore.list();
 });
 
-app.controller('EditCtrl', function($scope, $state){
-  $scope.note = angular.copy(getNote($state.params.noteId));
+app.controller('AddCtrl', function($scope, $state, NoteStore){
+  $scope.note = {
+    id: new Date().getTime().toString(),
+    title: '',
+    description: ''
+  }
 
   $scope.save = function(){
-    updateNote($scope.note);
+    NoteStore.create($scope.note);
     $state.go('list');
   }
 });
 
+app.controller('EditCtrl', function($scope, $state, NoteStore){
+  $scope.note = angular.copy(NoteStore.get($state.params.noteId));
 
+  $scope.save = function(){
+    NoteStore.update($scope.note);
+    $state.go('list');
+  }
+});
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
